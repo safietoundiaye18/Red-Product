7741285c7006185ffef34160842570e45345d939
safietoundiaye18 <safietoundiaye18@gmail.com>
2026-04-12 23:31:29 +0000

    Modification

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-console.log("Je suis connecté")
+console.log("Je suis connecté");
 
+const toggle = document.querySelector(".toggle-button");
+const sidebar = document.querySelector("#sidebar");
+const overlay = document.querySelector(".overlay");
 
+if (toggle && sidebar && overlay) {
 
+toggle.addEventListener("click", function () {
+    sidebar.classList.toggle("expand");
+    overlay.classList.toggle("active");
+});
 
-
-
-
-
-
-
-
-
+// Fermer la sidebar en cliquant sur l'overlay
+overlay.addEventListener("click", function () {
+    sidebar.classList.remove("expand");
+    overlay.classList.remove("active");
+});
+}
